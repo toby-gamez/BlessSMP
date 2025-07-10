27eec30e3b4f1018dd76e056c2210af8a5a0fb3c
toby-gamez <henematobik213@gmail.com>
2025-07-10 15:17:38 +0200

    Initial commit - clean Blazor pages and sryles

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using BlessSMP.Components;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
+public static class AppConfig
+{
+    public const string AppName = " - BlessSMP";
+}
